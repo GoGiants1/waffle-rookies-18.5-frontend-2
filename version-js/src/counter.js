8419a7be98d566738e01927e0604c5889de4342d
GoGiants1 <chooi221@snu.ac.kr>
2020-09-28 23:39:48 +0900

    js final

--- version-js/src/counter.js
+++ version-js/src/counter.js
@@ -1,15 +1,41 @@
-const number = document.getElementById("number");
+var number = document.getElementById("number");
 
 const increases = document.getElementsByClassName("increase");
+var listNum = document.getElementsByClassName("increase").length;
 
-const decrease = document.getElementById("decrease");
+var btns = document.getElementsByClassName("increase");
 
-increases.onclick.forEach(increse => {
+
+
+var count = 0;
+for(var i=0; i < (btns = document.getElementsByClassName("increase")).length;  i++){
+  btns[i].onclick = function(){
   
-  const current = parseInt(number.innerText, 10);
-  number.innerText = current + 1;
-});
+  count++;
+  number.innerHTML = count;
+
+  } 
+}
 
 
 
+
+// for (var i = 0; i < listNum; i++) {
+//   btns = document.getElementsByClassName("increase");
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+
+//     listNum = document.getElementsByClassName("increase").length;
+//     // If there's no active class
+//     if (current.length > 0) {
+//       current[0].className = current[0].className.replace(" active", "");
+//     }else{
+//       number = current.length;
+//     }
+
+//     console.log(btns.length)
+//     // Add the active class to the current/clicked button
+//     this.className += " active";
+//   });
+// }
 
